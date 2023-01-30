2736a9ef69bf0dcb713e24a637b950ea884ea8df
NIHALU <niihal.ucar@gmail.com>
2023-01-31 00:22:55 +0300

    Theme Chosen - V9

---
+++ NZHotel/NZHotel.DTOs/CustomerDtos/CustomerCreateDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NZHotel.DTOs.Interfaces;
+
+namespace NZHotel.DTOs.CustomerDtos
+{
+    public class CustomerCreateDto:IDto
+    {
+        public string ContactName { get; set; }
+        public string ContactSurname { get; set; }
+        public string ContactEmail { get; set; }
+        public string ContactPhone { get; set; }
+        public bool IsTurkish { get; set; }
+        public string TurkishIDNo { get; set; }
+        public string PassportNo { get; set; }
+        public string Country { get; set; }
+        public string Adress { get; set; }
+    }
+}
